07e9d683991db533c3eb69aa9f8193aaa31910cd
Daniel <nopestack@gmail.com>
2022-08-30 11:38:58 +0200

    move lib from main codebase (#1)

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+/// heavily inspired by https://github.com/carver/eth-trie.rs which is a fork of https://github.com/citahub/cita-trie
+///
+pub mod db;
+pub mod error;
+pub mod inner;
+pub mod result;
+pub mod trie;
+
+pub(crate) mod nibbles;
+pub(crate) mod node;
+pub mod op;
+
+pub use crate::op::*;
+pub use keccak_hash::H256;
